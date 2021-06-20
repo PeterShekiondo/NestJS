e8e9150ebfffadaf10144e955a3c6e9e9ab88b65
cefa david <cefasway@gmail.com>
2021-06-20 23:29:49 +0300

    :triangular_flag_on_post: Nestjs Pipes, Custom Pipes and Exception Handling

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform{
+    
+    readonly allowesStatuses = [TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE]
+
+    
+    private isStatusvalid (status){
+        const indx = this.allowesStatuses.indexOf(status)
+        return indx !== -1
+    }
+    
+    transform(value:any, metadata:ArgumentMetadata){        
+        if(!this.isStatusvalid(value)){
+            throw new BadRequestException('Invaliid provided status')
+        }
+        return value
+    }
+}
